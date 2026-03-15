f0c72cd8b226176d8282f31f4d9904bd07028052
Vika <suhonos.vika@gmail.com>
2026-03-15 22:40:22 +0200

    feat: add posthog proxy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-createApp(App).mount('#app')
+import posthog from 'posthog-js' 
+
+// 1. Ініціалізація PostHog 
+posthog.init('phc_cAity3uRcWX4CSMr5LgmmAm3UHQGelThexEf9CY43iF', {
+    api_host: window.location.origin + '/ingest',
+    person_profiles: 'identified_only'
+
+})
+
+// 2. Створення самого додатка Vue
+const app = createApp(App)
+app.mount('#app')
